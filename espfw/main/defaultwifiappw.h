8094ff0bdafe9a8c7d591714a9ffa49000e3b05c
Michael 'PoempelFox' Meier <git@mulder.franken.de>
2024-06-03 19:21:37 +0200

    default needs to be commented out to match documentation.

--- espfw/main/defaultwifiappw.h
+++ espfw/main/defaultwifiappw.h
@@ -8,7 +8,6 @@
  * do first-time configuration over unencrypted WiFi
  * with HTTP, thereby leaking secrets to anyone in WiFi
  * range.
- * so that you don't need to do first-time configuration
  * Please note however that this setting will ONLY be
  * used if you have never changed ANY setting on the
  * device, i.e. there is no config section in the flash
@@ -17,7 +16,7 @@
  * be 'open'.
  */
 
-#define DEFAULT_WIFI_AP_PW "secret"
+//#define DEFAULT_WIFI_AP_PW "secret"
 
 #endif /* _DEFWIFIAPPW_H_ */
 
